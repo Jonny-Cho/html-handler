6c65ec9437b038d22677a1db75a1efb5399a85f1
Jonny-Cho <andante2183@gmail.com>
2021-06-05 18:23:54 +0900

    refactor: add getResult method

--- src/main/java/com/wmp/htmlparser/domain/Separator.java
+++ src/main/java/com/wmp/htmlparser/domain/Separator.java
@@ -18,12 +18,16 @@ public class Separator {
 
         StringBuilder sbStr = new StringBuilder();
         StringBuilder sbNum = new StringBuilder();
-        while(matcher.find()) {
+        while (matcher.find()) {
             sbStr.append(matcher.group(1));
             sbNum.append(matcher.group(2));
         }
 
         sortedEnglish = Sorter.stringSort(sbStr.toString());
         sortedNumber = Sorter.numberSort(sbNum.toString());
+    }
+
+    public String getResult() {
+        return sortedEnglish + sortedNumber;
     }
 }
