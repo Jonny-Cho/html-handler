b86998eab1d17e0ea77b71cd8466a082882fdbf8
Jonny-Cho <andante2183@gmail.com>
2021-06-23 20:53:44 +0900

    chore: remove NoArgsConstructor

--- src/main/java/com/wmp/htmlparser/dto/ErrorResponseDto.java
+++ src/main/java/com/wmp/htmlparser/dto/ErrorResponseDto.java
@@ -1,19 +1,16 @@
 package com.wmp.htmlparser.dto;
 
 import com.wmp.htmlparser.ErrorCode;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class ErrorResponseDto {
 
-    private String message;
-    private int status;
-    private String code;
+    private final String message;
+    private final int status;
+    private final String code;
 
     public static ErrorResponseDto of(final ErrorCode errorCode, final String errorMessage) {
         return new ErrorResponseDto(errorMessage, errorCode.getStatus(), errorCode.getCode());
