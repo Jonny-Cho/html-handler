366af27446654d1cb000fd212d83d2c1ec2aee80
Jonny-Cho <andante2183@gmail.com>
2022-02-26 17:35:32 +0900

    refactor: setting swagger

--- src/main/java/com/wmp/htmlparser/controller/ApiParseController.java
+++ src/main/java/com/wmp/htmlparser/controller/ApiParseController.java
@@ -3,6 +3,9 @@ package com.wmp.htmlparser.controller;
 import com.wmp.htmlparser.dto.ParseRequestDto;
 import com.wmp.htmlparser.dto.ParseResponseDto;
 import com.wmp.htmlparser.service.ParseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,13 @@ public class ApiParseController {
 
     private final ParseService parseService;
 
+    @Operation(summary = "parse api", description = "URL 파싱 후 데이터 가공 하는 api")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+        @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+        @ApiResponse(responseCode = "500", description = "SERVER ERROR")
+    })
     @PostMapping("parse")
     public ResponseEntity<ParseResponseDto> parseWithoutHtml(@RequestBody @Valid ParseRequestDto parseRequestDto) {
         return new ResponseEntity<>(parseService.parse(parseRequestDto), HttpStatus.OK);
